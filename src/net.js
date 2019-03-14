d76be5fe7c44b51f08ae60c6010962d0e38cb5c2
Nikita Aristov <nikita.aristov@gmail.com>
2019-03-15 01:51:48 +0300

    spreadsheets API methods

--- src/net.js
+++ src/net.js
@@ -2,7 +2,16 @@
 import {google} from 'googleapis'
 import * as t from './types'
 
-export function fetchValues(auth: t.OAuth2Client, options): Promise<any> {
+export function fetchGUserInfo(auth: t.OAuth2Client): Promise<t.GUser> {
+  return new Promise(resolve => {
+    google.oauth2({version: 'v2', auth}).userinfo.get((err, res) => {
+      if (err) throw Error(err)
+      resolve(res.data)
+    })
+  })
+}
+
+export function fetchValues(auth: t.OAuth2Client, options: Object): Promise<any> {
   return new Promise(resolve => {
     google.sheets({version: 'v4', auth}).spreadsheets.values.get(options, (err, res) => {
       if (err) throw Error(err)
@@ -11,11 +20,29 @@ export function fetchValues(auth: t.OAuth2Client, options): Promise<any> {
   })
 }
 
-export function fetchGUserInfo(auth: t.OAuth2Client): Promise<t.GUser> {
+export function clearValues(auth: t.OAuth2Client, options: Object): Promise<any> {
   return new Promise(resolve => {
-    google.oauth2({version: 'v2', auth}).userinfo.get((err, res) => {
-      if (err) throw Error(err)
-      resolve(res.data)
+    google.sheets({version: 'v4', auth}).spreadsheets.values.clear(options, (err, res) => {
+      if (err) if (err) throw Error(err)
+      resolve(res.data.clearedRange)
+    })
+  })
+}
+
+export function appendValues(auth: t.OAuth2Client, options: Object): Promise<any> {
+  return new Promise(resolve => {
+    google.sheets({version: 'v4', auth}).spreadsheets.values.append(options, (err, res) => {
+      if (err) if (err) throw Error(err)
+      resolve(res.data.updates.updatedData)
+    })
+  })
+}
+
+export function updateValues(auth: t.OAuth2Client, options: Object): Promise<any> {
+  return new Promise(resolve => {
+    google.sheets({version: 'v4', auth}).spreadsheets.values.update(options, (err, res) => {
+      if (err) if (err) throw Error(err)
+      resolve(res.data.updatedData)
     })
   })
 }
