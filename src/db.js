dafad7babe2ec45698515d571d87f6129ebdf869
Gabriel Kobyz <gabrielkobyz@gmail.com>
2019-04-23 21:55:13 +0700

    cosmetics

--- src/db.js
+++ src/db.js
@@ -9,9 +9,9 @@ import * as u from './utils'
 const {DB_HOST, DB_NAME, POSTGRES_USER, POSTGRES_PASSWORD} = e.properties
 
 const config: t.ClientConfig = {
-  host: DB_HOST,
+  host    : DB_HOST,
   database: DB_NAME,
-  user: POSTGRES_USER,
+  user    : POSTGRES_USER,
   password: POSTGRES_PASSWORD,
 }
 const pool: t.Pool = new Pool(config)
